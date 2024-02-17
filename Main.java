536995d99f60e7defa6589e5443570d70f7f776d
Aditya Gupta <mr.aditya.gupta2001@gmail.com>
2024-02-17 22:44:39 +0530

    modified main class

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         CountDownTimer watch = new CountDownTimer(100);
         Thread thread = new Thread(watch);
+        System.out.println("hello world");
         thread.start();
     }
 }
-
